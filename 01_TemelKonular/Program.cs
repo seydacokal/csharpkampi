11b40ed9dd1793c4c86c806267627978b06392c4
Şeyda Çokal <seydacokal502@gmail.com>
2025-02-25 17:02:56 +0300

    my first commit

---
+++ 01_TemelKonular/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_TemelKonular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Yazdırma Komutları
+
+            //Console.Write("Merhaba Dünya");
+            //Console.WriteLine("Selam");
+
+            //Console.WriteLine("***** Yemek Kategorisi *****");
+            //Console.WriteLine();
+            //Console.WriteLine("1-Çorbalar");
+            //Console.WriteLine("2-Ana Yemekler");
+            //Console.WriteLine("3-Soğuk Başlangıçlar");
+            //Console.WriteLine("4-Salatalar");
+            //Console.WriteLine("5-Tatlılar");
+            //Console.WriteLine("6-İçecekler");
+            //Console.WriteLine();
+            //Console.WriteLine("***** Yemek Kategorisi *****");
+
+            #endregion
+
+            #region String Değişkenler
+
+            //string
+            //Değişken_türü değişken_adı;
+
+            //string name;
+            //name = "Murat";
+            //Console.Write(name);
+
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, distric, city;
+
+            //customerName = "Ali";
+            //customerSurname = "Çınar";
+            //customerPhone = "555 555 55 55";
+            //customerEmail = "deneme@gmail.com";
+            //distric = "Yunusemre";
+            //city = "Manisa";
+
+            //Console.WriteLine("**** Rezervasyon Kartı ****");
+            //Console.WriteLine();
+            //Console.WriteLine("--------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("Email Adresi: " + customerEmail);
+            //Console.WriteLine("Ades: " + distric + " " + city);
+            //Console.WriteLine("--------------------------");
+
+            //customerName = "Şeyda";
+            //customerSurname = "Çokal";
+            //customerPhone = "500 500 50 50";
+            //customerEmail = "test@gmail.com";
+            //distric = "Konak";
+            //city = "İzmir";
+
+            //Console.WriteLine("--------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("Email Adresi: " + customerEmail);
+            //Console.WriteLine("Ades: " + distric + " " + city);
+            //Console.WriteLine("--------------------------");
+
+
+
+
+
+
+
+
+            #endregion
+
+            #region Int Değişkenler
+
+            //int number = 24;
+            //Console.WriteLine(number);
+
+            //int hamburgerPrice = 300;
+            //int cokePrice = 35;
+            //int waterPrice = 10;
+            //int friesPrice = 50;
+            //int pizzaPrice = 250;
+            //int lemonadePrice = 30;
+
+            //Console.WriteLine("**** Restoran Menü Fiyatı ****");
+            //Console.WriteLine();
+            //Console.WriteLine("----Hamburger: " + hamburgerPrice + "TL");
+            //Console.WriteLine("----Pizza: " + pizzaPrice + "TL");
+            //Console.WriteLine("----Kola: " + cokePrice + "TL");
+            //Console.WriteLine("----Limonata: " + lemonadePrice + "TL");
+            //Console.WriteLine("----Kızartma: " + friesPrice + "TL");
+            //Console.WriteLine("----Su: " + waterPrice + "TL");
+            //Console.WriteLine();
+            //Console.WriteLine("**** Restoran Menü Fiyatı ****");
+
+            //Console.WriteLine();
+
+            //int hamburgerCount;
+            //int cokeCount;
+            //int waterCount;
+            //int friesCount;
+            //int pizzaCount;
+            //int lemonadeCount;
+
+            //int totalHamburgerPrice;
+            //int totalCokePrice;
+            //int totalWaterPrice;
+            //int totalFriesPrice;
+            //int totalPizzaPrice;
+            //int totalLemonadePrice;
+
+
+            //hamburgerCount = 3;
+            //cokeCount = 3;
+            //waterCount = 3;
+            //friesCount = 1;
+            //pizzaCount = 0;
+            //lemonadeCount = 0;
+
+            //totalHamburgerPrice = hamburgerCount * hamburgerPrice;
+            //totalCokePrice = cokeCount * cokeCount;
+            //totalWaterPrice = waterCount * waterPrice;
+            //totalFriesPrice = friesCount * friesPrice;
+            //totalPizzaPrice = pizzaCount * pizzaPrice;
+            //totalLemonadePrice = lemonadeCount * lemonadePrice;
+
+            //Console.WriteLine("------------------------------------------");
+            //Console.WriteLine("Hamburger Tutarı: " + totalHamburgerPrice + "TL");
+            //Console.WriteLine("Pizza Tutarı: " + totalPizzaPrice + "TL");
+            //Console.WriteLine("Su Tutarı: " + totalWaterPrice + "TL");
+            //Console.WriteLine("Kola Tutarı: " + totalCokePrice + "TL");
+            //Console.WriteLine("Limonata Tutarı: " + totalLemonadePrice + "TL");
+            //Console.WriteLine("Kızartma Tutarı: " + totalFriesPrice + "TL");
+
+            //Console.WriteLine();
+
+            //int totalPrice = totalCokePrice + totalWaterPrice + totalHamburgerPrice +
+            //    totalFriesPrice + totalPizzaPrice + totalLemonadePrice;
+
+            //Console.WriteLine("Toplam Ödenecek Tutar: " + totalPrice + "TL");
+
+            #endregion
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ 01_TemelKonular/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("01_TemelKonular")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("01_TemelKonular")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("0c4897d3-266e-4ec4-9c3e-fe9787918a64")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
